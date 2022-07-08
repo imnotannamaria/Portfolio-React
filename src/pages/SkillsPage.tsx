d32d78996fc5572c04f823ccace5821fd9ff5593
Anna Maria <aannamariabr@outlook.com>
2022-07-08 14:32:15 -0300

    Adicionando css na tela de carregamento.

--- src/pages/SkillsPage.tsx
+++ src/pages/SkillsPage.tsx
@@ -1,5 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
-import { BracketsCurly, Briefcase, Browser } from "phosphor-react";
+import { BracketsCurly, Briefcase, Browser, SpinnerGap } from "phosphor-react";
 import { Link } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
@@ -32,8 +32,8 @@ export function SkillsPage() {
 
     if(!data){
         return (
-            <div className="flex-1 flex justify-center items-center text-xl font-bold">
-                Carregando...✨
+            <div className="flex-1 gap-2 flex justify-center items-center text-2xl font-bold">
+                Loading <SpinnerGap size={30} weight="bold" />
             </div>
         )
     }

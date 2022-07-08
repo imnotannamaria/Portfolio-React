bccfaf4094a8b98c6782216a5ad8c3493c71b2b0
Anna Maria <aannamariabr@outlook.com>
2022-07-08 14:25:28 -0300

    Correção no css do componente Skills.

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,7 +5,7 @@ interface SkillsProps {
 
 export function Skills(props: SkillsProps){
     return (
-        <div className="flex flex-col gap-4 px-20 py-8 bg-purple-700 rounded-lg hover:bg-purple-400 transition-colors">
+        <div className="flex flex-col justify-center items-center gap-4 px-20 py-8 bg-purple-700 rounded-lg hover:bg-purple-400 transition-colors">
             <img src={props.imageUrl} alt="" />
 
             <span className="font-bold text-center">{props.title}</span>

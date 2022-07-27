30b62bcdc53664d306ed6fc5a5f961283e4df3f4
Anna Maria <aannamariabr@outlook.com>
2022-07-26 21:52:36 -0300

    Alterando estilização caso não tenha o link do site

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,27 +5,35 @@ interface ProjectsProps {
     title: string;
     description: string;
     githubLink: string;
-    siteLink: string;
+    siteLink?: string;
 }
 
 export function Project(props: ProjectsProps) {
+    const dontHaveSiteLink = !props.siteLink;
+    
     return (
         <div>
-                <img className="rounded w-full" src={props.imageUrl} alt="" />
+            <img className="rounded w-full" src={props.imageUrl} alt="" />
 
-                <div className="flex flex-col items-center bg-purple-700 pt-4 pb-2 px-4 gap-2">
-                    <span className="block text-xl font-bold">{props.title}</span>
-                    <span className="block text-sm">{props.description}</span>    
-                </div>
-                
-                <div className="flex justify-center gap-2 bg-purple-700 p-4 rounded-b-lg">
+            <div className="flex flex-col items-center bg-purple-700 pt-4 pb-2 px-4 gap-2">
+                <span className="block text-xl font-bold">{props.title}</span>
+                <span className="block text-sm">{props.description}</span>    
+            </div>
+            
+            <div className="flex justify-center gap-2 bg-purple-700 p-4 rounded-b-lg">
+                {dontHaveSiteLink ? 
+                    <a className="cursor-not-allowed bg-purple-300 p-4 rounded-full opacity-60">
+                        <Link size={25} />
+                    </a> :
                     <a href={props.siteLink} target="_blank" className="bg-purple-300 p-4 rounded-full hover:opacity-80 transition-opacity">
                         <Link size={25} />
-                    </a>
-                    <a href={props.githubLink} target="_blank" className="bg-purple-300 p-4 rounded-full hover:opacity-80 transition-opacity">
-                        <GithubLogo size={24} />
-                    </a>
-                </div>
+                    </a> 
+                }
+                
+                <a href={props.githubLink} target="_blank" className="bg-purple-300 p-4 rounded-full hover:opacity-80 transition-opacity">
+                    <GithubLogo size={24} />
+                </a>
             </div>
+        </div>
     )
 }
